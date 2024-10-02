8ba57245540ab76eaead04abda19dd2c686e723e
Diana Artavia <diana.artavia.da@gmail.com>
2024-10-02 10:36:46 -0600

    feat: model for connection to the API

---
+++ NexDevs/Models/NetworkAPI.cs
@@ -0,0 +1,16 @@
+﻿namespace NexDevs.Models
+{
+    public class NetworkAPI
+    {
+        public HttpClient Initial()
+        {
+            //objeto HttpClient
+            var client = new HttpClient();
+
+            //URL API
+            //client.BaseAddress = new Uri("https://localhost:7094/");
+
+            return client;
+        }
+    }
+}
